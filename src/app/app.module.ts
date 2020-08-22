4e7cdf45a20b445dd27dff6f933497975c492f9f
Prashant Nagori <Prashant.Nagori@mediakind.com>
2020-08-22 17:21:27 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,73 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventService,
+  EventsListResolverService,
+  EventRouteActivator,
+  CreateSessionComponent,
+  SessionListComponent,
+  DurationPipe
+
+
+} from './events/index'
+
+
+import {EventsAppComponent} from './events-app.component';
+import {Error404Component} from './Errors/404.component';
+import { JQ_TOKEN,TOASTR_TOKEN, Toastr, CollapsibleWellComponent, SimpleModalComponent, ModalTriggerDirective } from './common/index';
+import { AuthService } from './user/auth.service'
+import { NgModule } from '@angular/core';
+import { NavBarComponent } from './nav/navbar.component';
+import { appRoutes } from './routes';
+
+let toastr : Toastr =   window['toastr'];
+let jquery = window['$'];
 
 @NgModule({
   declarations: [
-    AppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component,
+    CreateSessionComponent,
+    SessionListComponent,
+    CollapsibleWellComponent,
+    DurationPipe,
+    SimpleModalComponent,
+    ModalTriggerDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [EventService, 
+    { provide : TOASTR_TOKEN, useValue: toastr}, 
+    { provide : JQ_TOKEN, useValue: jquery}, 
+    EventRouteActivator,
+    EventsListResolverService,
+    AuthService,
+    { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState }
+  ],
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component : CreateEventComponent) {
+
+  if(component.isDirty) {
+    return window.confirm('not saved')
+  }
+
+  return true
+}
